4d0b142443b747eb10527fa9bb1acac1769da3bd
Tatiana <efimova.tatalex@gmail.com>
2026-04-29 12:41:12 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,113 @@
+import { useState } from "react";
+import "./App.css";
+import { data } from "./data";
+
+function App () {
+  const [activities, setActivities] = useState(data);
+  const [index, setIndex] = useState(0);  
+  const [imgIndex, setImgIndex] = useState(0);
+  
+ 
+  const removeActivity = (id) => {
+    const newList = activities.filter(item => item.id !== id);
+    setActivities(newList);
+
+    if (index >= newList.length) {
+      setIndex(0);
+    }
+
+  }
+
+  const deleteAll = () => {
+    setActivities([]);
+  };
+
+  const nextItem = () => {
+    setIndex((prev) => {
+      let newIndex = prev + 1;
+      if (newIndex > activities.length - 1) newIndex = 0;
+      return newIndex;
+    });
+    setImgIndex(0);
+  };
+
+  const prevItem = () => {
+    setIndex((prev) => {
+      let newIndex = prev - 1;
+      if (newIndex < 0) newIndex = activities.length - 1;
+      return newIndex;
+    });
+    setImgIndex(0);
+  };
+
+  if (activities.length === 0) {
+    return (
+      <div className="empty">
+        <h1>No activities left</h1>
+        <button onClick={() => setActivities(data)}>Reset</button>
+      </div>
+    );
+  }
+
+  const safeIndex = index >= activities.length ? 0 : index;
+  const currentActivity = activities[index];
+  const { id, name, description, age, price, images } = activities[safeIndex];
+ 
+  const currentImages = images || [];
+  const safeImgIndex = imgIndex % currentImages.length;
+ 
+  
+  
+
+  const nextImage = () => {
+  setImgIndex((prev) => {
+    if (currentImages.length === 0) return 0;
+    return (prev + 1) % currentImages.length;
+    });
+  };
+
+  const prevImage = () => {
+  setImgIndex((prev) => {
+    if (currentImages.length === 0) return 0;
+    return prev === 0 ? currentImages.length - 1 : prev - 1;
+    });
+  };
+ 
+return (
+    <div>
+      <div className="container">
+        <h1 className="title">Kids Activities ({activities.length})</h1>
+      </div>
+
+      <div className="card">
+        <div className="image-wrapper">
+          <img src={currentImages[safeImgIndex]} alt={name} />
+       
+          <button className="img-btn left" onClick={prevImage}>‹</button>
+          <button className="img-btn right" onClick={nextImage}>›</button>
+        </div>
+
+        <h2>{name}</h2>
+        <p>{description}</p>
+
+        <p><strong>Age:</strong> {age}</p>
+        <p><strong>Price:</strong> {price}</p>
+
+        <button onClick={() => removeActivity(id)}>Remove</button>
+      </div>
+
+      <div className="btn">
+        <button onClick={prevItem}>Previous</button>
+        <button onClick={nextItem}>Next</button>
+      </div>
+
+      <div className="container">
+        <button onClick={deleteAll}>Delete All</button>
+      </div>
+    </div>
+  );
+ 
+}
+
+export default App;
+
---
+++ src/Data.js
@@ -0,0 +1,73 @@
+export const data = [
+  {
+    id: 1,
+    name: "Extreme Trampoline Park",
+    description: "Indoor trampoline park with foam pits, dodgeball, and kids zone.",
+    address: "50 Frank Nighbor Pl, Kanata, ON",
+    age: "All ages",
+    price: "$15–25 per session",
+    images: [
+      "/images/trampoline1.jpg",
+      "/images/trampoline2.jpg",
+      "/images/trampoline3.jpg"
+    ]
+  },
+
+  {
+    id: 2,
+    name: "Altitude Gym (Climbing)",
+    description: "Indoor climbing gym for beginners and advanced climbers.",
+    address: "501 Palladium Dr, Kanata, ON",
+    age: "6+",
+    price: "$20–30",
+    images: [
+      "/images/altitude1.jpg",
+      "/images/altitude2.jpg"
+    ]
+      },
+
+  {
+    id: 3,
+    name: "Valleyview Animal Farm",
+    description: "Interactive farm where kids can feed and see animals.",
+    address: "4750 Fallowfield Rd, Nepean",
+    age: "All ages",
+    price: "$10–15",
+    images: [
+      "/images/valleyviewfarm1.jpg",
+      "/images/valleyviewfarm2.jpg",
+      "/images/valleyviewfarm3.jpg",
+      "/images/valleyviewfarm4.jpg"
+    ]
+    
+  },
+
+  {
+    id: 4,
+    name: "Public Swimming Pools",
+    description: "Family-friendly public swimming options in Kanata and Stittsville.",
+    address: "Multiple locations",
+    age: "All ages",
+    price: "$3–6 per visit",
+    images: [
+      "https://img.athleticbusiness.com/files/base/abmedia/all/image/projects/2014/03/arch_FOM/2014/large/1064-RRCK-8.jpg?auto=format%2Ccompress&fit=crop&h=586&q=70&w=879",
+      "https://ottawa.ca/sites/default/files/styles/slider_desktop/public/2022-07/cap138627.jpg?itok=ommWhQzo",
+      "/images/pool3.jpg",
+    ]
+    
+  },
+
+  {
+    id: 5,
+    name: "Rideau Gymnastics (Resolute)",
+    description: "Gymnastics programs for kids with professional coaching.",
+    address: "Resolute Gymnastics, Ottawa",
+    age: "All ages",
+    price: "$15–25 per class",
+    images: [
+      "/images/rideau1.png",
+      "/images/rideau2.jpg",
+      "/images/rideau3.png",
+    ]
+  }
+];
